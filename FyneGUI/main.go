e073d94488da0e188f0632a7857aa891f7c4449c
John McSwain <john.i.mcswain@gmail.com>
2019-12-31 15:04:06 -0500

    adding files to repo

--- FyneGUI/main.go
+++ FyneGUI/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fyne.io/fyne/app"
+	"fyne.io/fyne/widget"
+)
+
 func main() {
-	$END$
+	app := app.New()
+
+	w := app.NewWindow("Hello")
+	w.SetContent(widget.NewVBox(
+		widget.NewLabel("Hello Fyne!"),
+		widget.NewButton("Quit", func() {
+			app.Quit()
+		}),
+	))
+
+	w.ShowAndRun()
 }
